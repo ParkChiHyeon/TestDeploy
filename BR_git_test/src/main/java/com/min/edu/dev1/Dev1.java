6748a7d70d913f8fc4992b962c36d673d7165b03
pch <pch@host.docker.internal>
2022-05-31 10:23:35 +0900

    dev1 class 작성

--- BR_git_test/src/main/java/com/min/edu/dev1/Dev1.java
+++ BR_git_test/src/main/java/com/min/edu/dev1/Dev1.java
@@ -2,4 +2,13 @@ package com.min.edu.dev1;
 
 public class Dev1 {
 
+	private String email;
+	private String name;
+	private String phone;
+	
+	public void testMethod() {
+		System.out.println("Hello World");
+	}
+	
+	
 }
