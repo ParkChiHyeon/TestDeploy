c96810ac2582e3f775035344980d879318e7e066
pch <pch@host.docker.internal>
2022-05-31 10:29:17 +0900

    Main, Test Method 실행

--- BR_git_test/src/main/java/com/min/edu/merge/MergeTest.java
+++ BR_git_test/src/main/java/com/min/edu/merge/MergeTest.java
@@ -1,5 +1,13 @@
 package com.min.edu.merge;
 
+import com.min.edu.dev2.Dev2;
+
 public class MergeTest {
 
+	public static void main(String[] args) {
+		Dev2 d2 = new Dev2();
+		d2.testMethod2();
+			
+	}
+	
 }
